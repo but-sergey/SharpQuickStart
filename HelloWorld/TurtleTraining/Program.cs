13beffd23130adf6ac4e5aa10a838b8f42fe060c
but-sergey <but-sergey@bk.ru>
2019-11-19 12:52:42 +0300

    Урок 4

---
+++ HelloWorld/TurtleTraining/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+            int i = 0;
+            while (i < 12)
+            {
+                Turtle.Move(100);
+                //Turtle.TurnRight();
+                Turtle.Turn(30);
+                i++;
+            }
+
+        }
+    }
+}
